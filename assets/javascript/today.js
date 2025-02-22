efc535ce74a7f4d805998cfe3a103849c45b80e2
STLwin2003 <shinthantlwin70@gmail.com>
2025-02-22 07:23:33 +0630

    creat list and add box by kyalkyal

---
+++ assets/javascript/today.js
@@ -0,0 +1,5 @@
+const creatbox = document.querySelector(".creatbox");
+const closeButton = document.querySelector(".closeButton");
+closeButton.onclick = () => {
+  creatbox.style.display = "none";
+};
